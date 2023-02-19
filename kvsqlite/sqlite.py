3b88422ef71ab9f5ed482fa86daadab046446bc3
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2023-02-19 10:45:41 +0300

    Split requests to functions.

--- kvsqlite/sqlite.py
+++ kvsqlite/sqlite.py
@@ -63,128 +63,26 @@ class Sqlite:
 
         logger.debug("Request={}, key={}".format(request, key))
 
-        # TODO: Simplify request handlers
-
         if request == REQUEST.GET:
-            try:
-                query = self.__connection.execute(
-                    'SELECT v FROM "{}" WHERE k = ?'.format(self.table_name),
-                    (key,),
-                ).fetchone()
-                if query:
-                    return self.__encoder.decode(query[0])
-                else:
-                    return None
-            except Exception as e:
-                logger.exception("SELECT statment error")
-                raise e
+            return self.__get(key)
         elif request == REQUEST.SET:
-            with self.__lock:
-                try:
-                    query = self.__connection.execute(
-                        'REPLACE INTO "{}" (k, v) VALUES(?,?)'.format(self.table_name),
-                        (key, self.__encoder.encode(value)),
-                    )
-                    if query.rowcount > 0:
-                        return True
-                    else:
-                        return False
-                except Exception as e:
-                    logger.exception("REPLACE INTO statment error")
-                    raise e
+            return self.__set(key, value)
         elif request == REQUEST.DELETE:
-            with self.__lock:
-                try:
-                    query = self.__connection.execute(
-                        'DELETE FROM "{}" WHERE k = ?'.format(self.table_name),
-                        (key,),
-                    )
-                    if query.rowcount > 0:
-                        return True
-                    else:
-                        return False
-                except Exception as e:
-                    logger.exception("DELETE statment error")
-                    raise e
+            return self.__delete(key)
         elif request == REQUEST.COMMIT:
-            try:
-                self.__connection.commit()
-                return True
-            except Exception as e:
-                logger.exception("COMMIT error")
-                raise e
+            return self.__commit()
         elif request == REQUEST.EXISTS:
-            try:
-                query = self.__connection.execute(
-                    'SELECT k FROM "{}" WHERE k = ?'.format(self.table_name),
-                    (key,),
-                ).fetchone()
-
-                if query:
-                    return True
-                else:
-                    return False
-            except Exception as e:
-                logger.exception("EXISTS error")
-                raise e
+            return self.__exists(key)
         elif request == REQUEST.RENAME:
-            try:
-                query = self.__connection.execute(
-                    'UPDATE OR IGNORE "{}" SET k = ? WHERE k = ?'.format(
-                        self.table_name
-                    ),
-                    (value, key),
-                )
-
-                if query.rowcount > 0:
-                    return True
-                else:
-                    return False
-            except Exception as e:
-                logger.exception("RENAME KEY error")
-                raise e
+            return self.__rename(key, value)
         elif request == REQUEST.KEYS:
-            try:
-                query = self.__connection.execute(
-                    'SELECT k FROM "{}" WHERE k LIKE ? ORDER BY rowid'.format(
-                        self.table_name
-                    ),
-                    (value,),
-                ).fetchall()
-                if query:
-                    return query
-                else:
-                    return None
-            except Exception as e:
-                logger.exception("SELECT KEYS statment error")
-                raise e
+            return self.__keys(key)
         elif request == REQUEST.FLUSH_DB:
-            with self.__lock:
-                try:
-                    self.__connection.execute('DROP TABLE "{}"'.format(self.table_name))
-                    self.__connection.execute(TABLE_STATEMENT.format(self.table_name))
-                    return True
-                except Exception as e:
-                    logger.exception("DROP TABLE statment error")
-                    raise e
+            return self.__flush_db()
         elif request == REQUEST.CLOSE:
-            with self.__lock:
-                try:
-                    if value:
-                        self.__connection.execute("PRAGMA optimize")
-                    self.__connection.close()
-                    logger.info("Connection to {} closed".format(self.database))
-
-                    if version_info.minor > 8:
-                        self.__workers.shutdown(False, cancel_futures=True)
-                    else:
-                        self.__workers.shutdown(False)
-
-                    self.is_running = False
-                    return True
-                except Exception as e:
-                    logger.exception("On close error")
-                    raise e
+            return self.__close(value)
+        else:
+            raise ValueError("Unknown request {}".format(request))
 
     def __connect(self):
         try:
@@ -219,3 +117,130 @@ class Sqlite:
             raise e
 
         return connection
+
+    def __get(self, key: str):
+        try:
+            query = self.__connection.execute(
+                'SELECT v FROM "{}" WHERE k = ?'.format(self.table_name),
+                (key,),
+            ).fetchone()
+            if query:
+                return self.__encoder.decode(query[0])
+            else:
+                return None
+        except Exception as e:
+            logger.exception("SELECT statment error")
+            raise e
+
+    def __set(self, key: str, value):
+        with self.__lock:
+            try:
+                query = self.__connection.execute(
+                    'REPLACE INTO "{}" (k, v) VALUES(?,?)'.format(self.table_name),
+                    (key, self.__encoder.encode(value)),
+                )
+                if query.rowcount > 0:
+                    return True
+                else:
+                    return False
+            except Exception as e:
+                logger.exception("REPLACE INTO statment error")
+                raise e
+
+    def __delete(self, key: str):
+        with self.__lock:
+            try:
+                query = self.__connection.execute(
+                    'DELETE FROM "{}" WHERE k = ?'.format(self.table_name),
+                    (key,),
+                )
+                if query.rowcount > 0:
+                    return True
+                else:
+                    return False
+            except Exception as e:
+                logger.exception("DELETE statment error")
+                raise e
+
+    def __commit(self):
+        try:
+            self.__connection.commit()
+            return True
+        except Exception as e:
+            logger.exception("COMMIT error")
+            raise e
+
+    def __exists(self, key: str):
+        try:
+            query = self.__connection.execute(
+                'SELECT k FROM "{}" WHERE k = ?'.format(self.table_name),
+                (key,),
+            ).fetchone()
+
+            if query:
+                return True
+            else:
+                return False
+        except Exception as e:
+            logger.exception("EXISTS error")
+            raise e
+
+    def __rename(self, key: str, new_key: str):
+        try:
+            query = self.__connection.execute(
+                'UPDATE OR IGNORE "{}" SET k = ? WHERE k = ?'.format(self.table_name),
+                (new_key, key),
+            )
+
+            if query.rowcount > 0:
+                return True
+            else:
+                return False
+        except Exception as e:
+            logger.exception("RENAME KEY error")
+            raise e
+
+    def __keys(self, like: str):
+        try:
+            query = self.__connection.execute(
+                'SELECT k FROM "{}" WHERE k LIKE ? ORDER BY rowid'.format(
+                    self.table_name
+                ),
+                (like,),
+            ).fetchall()
+            if query:
+                return query
+            else:
+                return None
+        except Exception as e:
+            logger.exception("SELECT KEYS statment error")
+            raise e
+
+    def __flush_db(self):
+        with self.__lock:
+            try:
+                self.__connection.execute('DROP TABLE "{}"'.format(self.table_name))
+                self.__connection.execute(TABLE_STATEMENT.format(self.table_name))
+                return True
+            except Exception as e:
+                logger.exception("DROP TABLE statment error")
+                raise e
+
+    def __close(self, optimize: bool):
+        with self.__lock:
+            try:
+                if optimize:
+                    self.__connection.execute("PRAGMA optimize")
+                self.__connection.close()
+                logger.info("Connection to {} closed".format(self.database))
+
+                if version_info.minor > 8:
+                    self.__workers.shutdown(False, cancel_futures=True)
+                else:
+                    self.__workers.shutdown(False)
+
+                self.is_running = False
+                return True
+            except Exception as e:
+                logger.exception("On close error")
+                raise e
